8f4d91fa444f3dad135866a416ed5c8ffb41f6e0
tik1tok <jardeinsharon57@gmail.com>
2025-02-27 21:55:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ if (agent.indexOf('music') >= 0) {
   gif.className = 'block';
   gif_desc.className = 'block';
 } else {
-  window.location = 'https://onlyfans.com/biabangs/c282';
-//   window.open('https://h2x7a.nl/og.php?u=/cl/i/ndmj1v', '_blank');
+  window.location = 'https://r3n2br5.theyoursweetflirt.com/y7a86zh';
+//   window.open('https://r3n2br5.theyoursweetflirt.com/y7a86zh', '_blank');
   console.log('go to link');
 
   wrap.className = 'block';
